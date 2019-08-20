ce53a7c6580bfe5f663d83e828583b050b5790a0
Khidir Yusoff <khidir.mybb@gmail.com>
2019-08-20 12:42:36 +0800

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,4 +1,15 @@
-  var time = "12:12:12PM"
+/*
+Given a time in -hour AM/PM format, convert it to military (-hour) time.
+Note: Midnight is on a -hour clock, and on a -hour clock. Noon is on a -hour clock, and on a -hour clock.
+Input Format
+A single string containing a time in -hour clock format (i.e.: or ), where 00 <= h <= 23 and 00 <= m <= 59.
+Output Format
+Convert and print the given time in -hour format, where .
+Sample Input = Sample Output
+07:05:45PM = 19:05:45 12:05:45AM = 00:05:45 12:05:45PM = 12:05:45
+*/  
+
+var time = "12:12:12PM"
   var newTime, i, j;
 
   document.write(time," = ");
